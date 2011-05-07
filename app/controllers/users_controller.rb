604a35800d541962de122b138f3e689ced5bfd9f
Chien Hsiao <chienh@gmail.com>
2011-05-06 21:12:45 -0300

    Done with user edit/update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+  before_filter :signed_in_user, :only => [:new, :create]
+  before_filter :protect_from_deleting_itself, :only => :destroy
+  
   def new
     @user = User.new
     @title = "Sign up"
+  end
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
 
   def show
@@ -22,4 +33,50 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    #@user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+
+  private
+    
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    def signed_in_user
+      redirect_to(root_path) unless !signed_in?
+    end
+
+    def protect_from_deleting_itself
+      @user = User.find(params[:id])
+      redirect_to root_path unless !current_user?(@user)
+    end
 end
